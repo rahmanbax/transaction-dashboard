466182c964bb929cef0610c4a77123f05642dfe9
rahmanbax <basyirahman@gmail.com>
2025-02-01 04:54:14 +0700

    feat: enhance AddDataPage with NavBar and FormsAdd components for improved transaction entry

--- src/pages/AddDataPage.jsx
+++ src/pages/AddDataPage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import FormsAdd from "../components/FormsAdd";
+import { getTransactions } from "../services/transaction.service";
 
 const AddDataPage = () => {
   return (
-    <div>
-      Add
+    <div className="flex">
+      <NavBar />
+      <main className="my-10 mx-10 w-full h-fit ">
+        <h1 className="font-semibold text-3xl">Tambah Transaksi</h1>
+        <div className="w-full mt-10 p-4 bg-white border-1 border-gray-200 shadow-md">
+          <FormsAdd />
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default AddDataPage
+export default AddDataPage;
