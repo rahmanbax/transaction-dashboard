799cf6423b71ad79219655f27954f5c535ec4027
rahmanbax <basyirahman@gmail.com>
2025-02-01 04:53:33 +0700

    feat: update routing to use Dashboard component and add alternative dashboard route

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
+import HomePage from "./pages/Dashboard.jsx";
 import AddDataPage from "./pages/AddDataPage.jsx";
 import EditDataPage from "./pages/EditDataPage.jsx";
+import DashboardFromJson from "./pages/DashboardFromJson.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/dashboardalt",
+    element: <DashboardFromJson />,
   },
   {
     path: "/add",
