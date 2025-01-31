ca624f32f36c0d6123327b5bfeda0c378016a982
rahmanbax <basyirahman@gmail.com>
2025-02-01 04:54:27 +0700

    feat: enhance EditDataPage with NavBar and FormsEdit components for improved transaction editing

--- src/pages/EditDataPage.jsx
+++ src/pages/EditDataPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/NavBar";
+import FormsEdit from "../components/FormsEdit";
 
 const EditDataPage = () => {
   return (
-    <div>
-      Edit
+    <div className="flex">
+      <NavBar />
+      <main className="my-10 mx-10 w-full h-fit ">
+        <h1 className="font-semibold text-3xl">Edit Transaksi</h1>
+        <div className="w-full mt-10 p-4 bg-white border-1 border-gray-200 shadow-md">
+          <FormsEdit />
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default EditDataPage
+export default EditDataPage;
