d92602ba9ef0dc46a11f41a321824830628f14b6
rahmanbax <basyirahman@gmail.com>
2025-01-31 19:17:50 +0700

    feat: enhance NavBar with Dashboard link and styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import { MdDashboard } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul><li>
-        </li></ul>
+    <nav className="sticky left-0 top-0 w-56 h-screen bg-white border-r border-r-gray-200">
+      <ul className="mt-10">
+        <Link to={"/"}><li className="p-5 hover:bg-gray-100 text-base flex gap-2 items-center"><MdDashboard/> Dashboard</li></Link>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
